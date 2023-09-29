d745c71aaa96468507a4eb80db9a1e0721ede31f
Paul Njuga <paulnjuga@gmail.com>
2023-09-29 23:04:24 +0300

    Add explicit get method to status route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def status():
     """Returns status of the API"""
     return jsonify({'status': 'OK'})
